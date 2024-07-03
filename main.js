79212c115f93ae81e96c112c62edcad191704b13
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-07-03 20:11:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 const https = require('https');
 
-// TODO: Implement this function
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos/1';
+
+https.get(apiUrl, (response) => {
+  let data = '';
+
+  response.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  response.on('end', () => {
+    try {
+      const todo = JSON.parse(data);
+      console.log(todo.title);
+    } catch (error) {
+      console.error('Error parsing JSON:', error.message);
+    }
+  });
+
+}).on('error', (error) => {
+  console.error('Error fetching todo:', error.message);
+});
